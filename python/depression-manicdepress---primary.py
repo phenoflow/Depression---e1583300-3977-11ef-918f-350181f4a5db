555e34a7afb954d6e0a02bf46d89dafbb6d43b70
Martin Chapman <contact@martinchapman.co.uk>
2024-07-03 21:10:51 +0100

    Update made by Phenoflow

---
+++ python/depression-manicdepress---primary.py
@@ -0,0 +1,25 @@
+# South East London (SEL) Long COVID Programme, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"310497006","system":"snomedct"},{"code":"36474008","system":"snomedct"},{"code":"28475009","system":"snomedct"},{"code":"765176007","system":"snomedct"},{"code":"231494001","system":"snomedct"},{"code":"13746004","system":"snomedct"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('depression-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["depression-manicdepress---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["depression-manicdepress---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["depression-manicdepress---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
